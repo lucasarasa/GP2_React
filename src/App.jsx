33ef9c7bbf2ad7face60172e4e16118148cf54cd
Lucas Sarasa <lucasmsarasa@gmail.com>
2024-11-04 19:15:59 -0300

    Subindo login/cadastro funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { BrowserRouter, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
 import { Login } from './pages/Login/Login'
 import { Home } from './pages/HomePage/Home'
+import { Cadastro } from './pages/Cadastro/Cadastro'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Login}/>
           <Route exact path='/home' component={Home}/>
+          <Route exact path='/cadastro' component={Cadastro}/>
         </Switch>
       </BrowserRouter>
     </>
