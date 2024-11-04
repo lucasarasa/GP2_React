dca083400376a028123c91d05f7326935a61c021
Lucas Sarasa <lucasmsarasa@gmail.com>
2024-11-04 19:43:48 -0300

    Ajeitando merge

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import { PrivateRoute } from './component/PrivateRoute';
 import { LoginComponent } from './components/componentLogin/LoginComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
 
