a6605c525af306212cb6b79d337239d27c53e4a7
Lucas Sarasa <lucasmsarasa@gmail.com>
2024-11-04 21:19:14 -0300

    Subindo alterações

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Cadastro } from './pages/Cadastro/Cadastro'
 import { Navbar } from './component/Navbar';
 import { PrivateRoute } from './component/PrivateRoute';
 import { LoginComponent } from './component/componentLogin/LoginComponent'
-import { ProdutosMasculinos, ProdutosFemininos, ProdutosInfantis, Calcados } from './pages';
+// import { ProdutosMasculinos, ProdutosFemininos, ProdutosInfantis, Calcados } from './pages';
 
 function App() {
 
@@ -17,10 +17,10 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route path='/login' component={Login} />
         <Route path='/cadastro' component={Cadastro} />
-        <Route path='/masculino' component={ProdutosMasculinos} />
+        {/* <Route path='/masculino' component={ProdutosMasculinos} />
         <Route path='/feminino' component={ProdutosFemininos} />
         <Route path='/infantil' component={ProdutosInfantis} />
-        <Route path='/calcados' component={Calcados} />
+        <Route path='/calcados' component={Calcados} /> */}
       </Switch>
     </BrowserRouter>
   );
