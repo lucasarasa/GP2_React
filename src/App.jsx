0dd46f7553f3116ab4f1fca8415b46f231cfb115
AlineMG14 <alinemgoncalves@hotmail.com>
2024-11-05 11:17:19 -0300

    Atualização do App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
-import { Home } from './pages/HomePage/Home';
-import { Login } from './pages/Login/Login';
-import { Cadastro } from './pages/Cadastro/Cadastro';
-import { RoupasMasculinas } from './pages/RoupasMasculinas/RoupasMasculinas';
-import { Carrinho } from './components/carrinho/Carrinho';
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/HomePage/Home";
+import { Login } from "./pages/Login/Login";
+import { Cadastro } from "./pages/Cadastro/Cadastro";
+import { RoupasMasculinas } from "./pages/RoupasMasculinas/RoupasMasculinas";
+import { RoupasFemininas } from "./pages/RoupasFemininas/RoupasFemininas";
+import { Carrinho } from "./components/carrinho/Carrinho";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Switch>
-      <Route path='/masculino' component={RoupasMasculinas} />
+        <Route path="/masculino" component={RoupasMasculinas} />
+        <Route path="/feminino" component={RoupasFemininas} />
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/cadastro" component={Cadastro} />
