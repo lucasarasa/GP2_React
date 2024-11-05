10a5f7cb36de84d4048343120d4824af70eba0cc
Joaogabrielgabry <joaogabrielgabry@gmail.com>
2024-11-05 13:32:13 -0300

    RoupasMasculinas

--- src/pages/RoupasMasculinas/RoupasMasculinas.jsx
+++ src/pages/RoupasMasculinas/RoupasMasculinas.jsx
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../api/api';
 import './RoupasMasculinas.css';
 
 const RoupasMasculinas = () => {
     const [selectedProduct, setSelectedProduct] = useState(null);
     const [zoomed, setZoomed] = useState(false);
+    const [products, setProducts] = useState([]);
 
-    const products = [
-        { id: 1, name: "Camisa em Tricoline Acetinada com Gola Padre Branco", price: "R$ 79,90", image: '/src/Roupas-Categorias/Masculino/1.webp' },
-        { id: 2, name: "Bermuda Esportiva com Cordão Interno e Lettering Refletivo Azul Marinho", price: "R$ 129,90", image: '/src/Roupas-Categorias/Masculino/2.webp' },
-        { id: 3, name: "Camiseta Polo Comfort em Algodão Peruano Preto", price: "R$ 299,90", image: '/src/Roupas-Categorias/Masculino/3.webp' },
-        { id: 4, name: "Bermuda em Microfibra com Cós Elástico e Estampa Floral de Girassol Preto", price: "R$ 149,90", image: '/src/Roupas-Categorias/Masculino/4.webp' },
-        { id: 5, name: "Camisa Comfort em Algodão com Estampa Xadrez e Manga Longa Azul/ Preto/ Off White", price: "R$ 49,90", image: '/src/Roupas-Categorias/Masculino/5.webp' },
-        { id: 6, name: "Calça Loose em Jeans com Bolsos Azul", price: "R$ 89,90", image: '/src/Roupas-Categorias/Masculino/6.webp' },
-        { id: 7, name: "Polo com Gola Lapela sem Botão e Textura Canelada Vermelho Vinho", price: "R$ 159,90", image: '/src/Roupas-Categorias/Masculino/7.webp' },
-        { id: 8, name: "Calça Wide Alfaiataria com Cós Bege", price: "R$ 79,90", image: '/src/Roupas-Categorias/Masculino/8.webp' },
-        { id: 9, name: "Regata Regular Esportiva com Tecnologia Dry e Estampa Heat Print Verde", price: "R$ 99,90", image: '/src/Roupas-Categorias/Masculino/9.webp' },
-        { id: 10, name: "Casaco Básico em Moletom com Capuz e Zíper Frontal Preto", price: "R$ 199,90", image: '/src/Roupas-Categorias/Masculino/10.webp' },
-    ];
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await api.get('/produtos');
+                setProducts(response.data);
+            } catch (error) {
+                console.error("Erro ao buscar produtos:", error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
 
     const handleProductClick = (product) => {
         setSelectedProduct(product);
@@ -46,10 +48,10 @@ const RoupasMasculinas = () => {
                 <div className="product-list">
                     {products.map(product => (
                         <div key={product.id} className="product-card" onClick={() => handleProductClick(product)}>
-                            <img src={product.image} alt={product.name} className="product-image" />
+                            <img src={product.imgurl} alt={product.nome} className="product-image" />
                             <div className="product-info">
-                                <h3 className="product-name">{product.name}</h3>
-                                <p className="product-price">{product.price}</p>
+                                <h3 className="product-name">{product.nome}</h3>
+                                <p className="product-price">R$ {product.preco}</p>
                             </div>
                         </div>
                     ))}
@@ -60,14 +62,14 @@ const RoupasMasculinas = () => {
                 <div className="popup-overlay" onClick={closePopup}>
                     <div className="popup-content" onClick={(e) => e.stopPropagation()}>
                         <img 
-                            src={selectedProduct.image} 
-                            alt={selectedProduct.name} 
+                            src={selectedProduct.imgurl} 
+                            alt={selectedProduct.nome} 
                             className={`popup-image ${zoomed ? 'zoomed' : ''}`} 
                             onClick={toggleZoom}
                         />
                         <div className="popup-details">
-                            <h2>{selectedProduct.name}</h2>
-                            <p>{selectedProduct.price}</p>
+                            <h2>{selectedProduct.nome}</h2>
+                            <p>R$ {selectedProduct.preco}</p>
 
                             <div className="size-selection">
                                 <h3>Selecione o tamanho:</h3>
