d050feb06329370df1fb2faecd80ba78c13f16c1
Joaogabrielgabry <joaogabrielgabry@gmail.com>
2024-11-06 20:28:51 -0300

    Revert "avaliação de produtos"
    This reverts commit 6cb7ec8d046cc68f0ba00fa0f3f4235f5f0289f3.

--- src/pages/RoupasMasculinas/RoupasMasculinas.jsx
+++ src/pages/RoupasMasculinas/RoupasMasculinas.jsx
@@ -3,18 +3,12 @@ import { api } from "../../api/api";
 import "./RoupasMasculinas.css";
 import { useEffect } from "react";
 import { CartContext } from "../../contexts/CartContext";
-import CommentsSection from "../../components/CommentsSection";
-import Post from "../../components/Post";
-
 
 const RoupasMasculinas = () => {
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [zoomed, setZoomed] = useState(false);
   const [products, setProducts] = useState([]);
   const { addToCart } = useContext(CartContext);
-  const [posts, setPosts] = useState([]);
-  const [autor, setAutor] = useState("");
-  const [comentario, setComentario] = useState("");
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -31,14 +25,6 @@ const RoupasMasculinas = () => {
     fetchProducts();
   }, []);
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault()
-
-    const novoPost = { autor, comentario, likes: 0 }
-    setPosts([...posts, novoPost])
-    setAutor('')
-    setComentario('')
-  }
   const handleProductClick = (product) => {
     setSelectedProduct(product);
   };
@@ -55,6 +41,7 @@ const RoupasMasculinas = () => {
     e.preventDefault();
     alert("Você está inscrito na newsletter!");
   };
+
   return (
     <section className="mens-clothing">
       <h1 className="page-title">Roupas Masculinas</h1>
@@ -97,15 +84,6 @@ const RoupasMasculinas = () => {
               className={`popup-image ${zoomed ? "zoomed" : ""}`}
               onClick={toggleZoom}
             />
-            <CommentsSection
-              posts={posts}
-              setPosts={setPosts}
-              autor={autor}
-              setAutor={setAutor}
-              comentario={comentario}
-              setComentario={setComentario}
-            />
-
             <div className="popup-details">
               <h2>{selectedProduct.nome}</h2>
               <p>R$ {selectedProduct.preco}</p>
